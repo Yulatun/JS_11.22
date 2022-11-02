70f8585b217dd1af7351cf9a228c7de6ca6a7412
Yulatun <julialimbekov@gmail.com>
2022-11-02 16:34:35 +0200

    [task_1,task_2]: finished the tasks

---
+++ 2.js
@@ -0,0 +1,14 @@
+let symbols = prompt('enter less than 3 symbols');
+let number = parseFloat(prompt('enter a number'));
+
+let stringToReturn = ((symbols + ' ').repeat(number) + '\n').repeat(number);
+
+if (
+  symbols.length <= 3 && 
+  symbols.length > 0 && 
+  0 < number && 
+  number <= 10) {
+  console.log(stringToReturn);
+} else {
+  console.log('Incorrect input!');
+}
