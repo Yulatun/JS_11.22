5f5cb9a0e3c0de864847b3f092c3eef5c3a801cd
Yulatun <julialimbekov@gmail.com>
2022-11-06 22:32:48 +0200

    [task_2]:correct mistakes

--- 2.js
+++ 2.js
@@ -1,7 +1,7 @@
-let symbols = prompt('enter less than 3 symbols');
-let number = parseFloat(prompt('enter a number'));
+const symbols = prompt('enter less than 3 symbols');
+const number = parseFloat(prompt('enter a number'));
 
-let stringToReturn = ((symbols + ' ').repeat(number) + '\n').repeat(number);
+const stringToReturn = ((symbols + ' ').repeat(number) + '\n').repeat(number);
 
 if (symbols.length <= 3 && symbols.length > 0 && 0 < number && number <= 10) {
   console.log(stringToReturn);
